2725289939f3c37bfbdac6391dac479ff73c2b23
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-29 13:32:59 +0530

    Implement LCM calculation using recursion
    Refactor LCM calculation into a separate function.

--- LCM.c
+++ LCM.c
@@ -1,24 +1,30 @@
 #include<stdio.h>
+int lcm_n(int,int,int);
 int main(){
     int a,b,max;
     printf("Enter a and b value : ");
     scanf("%d %d",&a,&b);
     if(a>b){
+      
         max=a;
-    }
+       }
     else{
-        max=b;
+                max=b;
     }
-    while(1){
-        if(max%a==0 || max%b==0){
-max++;
-            printf("LCM of %d and %d is %d",a,b,max);
-            break;
-        }
-        else{
-            break;
-        }
-        
-    }
-    
+    int c=lcm_n(max,a,b);
+    printf("%d %d LCM is %d",a,b,c);
+return 0;
 }
+int lcm_n(int max,int a,int b){
+    if(max%a==0&&max%b==0){
+       
+return max;
+    }
+else{
+    max++;
+return lcm_n(max,a,b);
+    }
+}
+
+
+   
